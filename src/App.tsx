31ef40936dcde794f7dc968beba7b9ac835e3501
Adel Ansari <adelansari.a@gmail.com>
2024-04-07 16:07:15 +0300

    Add Footer and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+
 function App() {
   return (
     <>
-      <div>Hello World</div>
+      <Header title='React Contact' />
+      <Footer />
     </>
   );
 }
